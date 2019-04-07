3b0ddde8badfba2716874dd616445ab3f6eca613
Administrator <admin@example.com>
2019-04-07 10:55:18 +0700

    Update script

--- sonarlocal.sh
+++ sonarlocal.sh
@@ -2,7 +2,7 @@
 # token="$(cat sonar.txt)"
 dir="$(pwd)"
 # https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=./lcov calculation.tests/calculation.tests.csproj
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=./lcov Number.Tests/Number.Tests.csproj
 dotnet build-server shutdown
 dotnet sonarscanner begin /k:"Number" /d:sonar.host.url="http://localhost:9000" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml"
 dotnet build
